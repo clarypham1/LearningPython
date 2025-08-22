91c3ffd975a9778fafac6c206b5755aa98a87ba0
tranpham <clary.phamvb@gmail.com>
2025-08-22 15:42:56 +0300

    first

--- hello.py
+++ hello.py
@@ -1,1 +1,28 @@
-print ("Hello, Tran Pham")
+
+#Types of variables based on data types
+# 1. numeric data types:
+#int: whole number
+#float: decimal
+#complex: real + imaginary numbers
+
+#2. string
+#3. Booleom Tre/False
+#4. tuple ()
+#5. dictionary
+#6. list
+#7. set
+
+programming = float(input("Enter your programming score: "))
+print (programming)
+physics = float(input("Enter your physics score: "))
+print (physics)
+mathematics = float(input("Enter your mathematics score: "))
+print (mathematics)
+
+percentage1 = int(programming/(physics+programming+mathematics)*100)
+print ("The percentage of programming score is:", percentage1, "%")
+percentage2 = int(physics/(physics+programming+mathematics)*100)
+print ("The percentage of physics score is:", percentage2, "%")
+percentage3 = int(mathematics/(physics+programming+mathematics)*100)
+print ("The percentage of mathematics score is:", percentage3, "%")
+
